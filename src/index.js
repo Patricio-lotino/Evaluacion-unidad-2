bcd0556af830a13105b25b88b2aba6c53a3b97e4
Patricio Cárdenas <patricio.cardenas.palma@ciisa.cl>
2022-04-23 16:31:29 -0400

    Prueba fallida

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ require('dotenv').config()
 
 const app = require('./app')
 
-// eslint-disable-next-line no-unused-expressions
-process.env.DATABASE_URL
-
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
